575f2538bd2f1fc79a0186a20979facd663b40bf
Sebastian Graz <sebastian@Sebastians-MacBook-Pro-2.local>
2016-12-15 13:53:23 +0000

    fading menu added

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -12,6 +12,36 @@ $doc.ready(function() {
       })
     })
 
+
+    $(function() {
+      //caches a jQuery object containing the header element
+      var header = $('.navigation');
+      var caseHeader = $('.case-header').outerHeight();
+      var calcHeader = caseHeader - ( header.outerHeight() + header.position().top ) ;
+
+
+
+      $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= calcHeader) {
+          header.addClass('hidden');
+        } else {
+          header.removeClass('hidden');
+        }
+      });
+    });
+
+    // $(window).on('scroll', function () {
+    //   var windowTop = $(window).scrollTop();
+    //   if (windowTop >= 200) {
+    //     console.log('scrolled 200 px');
+    //   }
+    //   else {
+    //
+    //   }
+    // });
+
     $('.tile-bg').each( function(){
       var bgcolor = $(this).data('bgcolor');
       $(this).css({
@@ -27,7 +57,7 @@ $doc.ready(function() {
 
     $('.tile').each( function(){
       var lspacer = $(this).data('lspacer'),
-          rspacer = $(this).data('rspacer');
+      rspacer = $(this).data('rspacer');
 
       $(this).addClass('lspacer-' + lspacer + ' rspacer-' + rspacer)
     });
