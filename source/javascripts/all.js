ec1df5c4baabaa3706b915f041580db94b8b6c5a
Sebastian Graz <sebastian@Sebastians-MacBook-Pro-2.local>
2016-12-16 14:27:27 +0000

    testing spacers

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -26,11 +26,47 @@ $doc.ready(function() {
       $(this).removeClass('expanded');
     });
 
+
+    function get_position(e) {
+      var res = new Object();
+      var pos = e.position();
+      res.left = pos.left;
+      res.top = pos.top;
+      res.right = res.left + e.width();
+      res.bottom = res.top + e.height();
+      return res;
+    }
+
+    setTimeout(function () {
+      $('.tile').each( function(){
+        $(this).addClass( 'top-' + get_position($(this)).top)
+        $(this).addClass( 'left-' + get_position($(this)).left)
+        $(this).addClass( 'right-' + get_position($(this)).right)
+        $(this).addClass( 'bot-' + get_position($(this)).bottom)
+        console.log();
+
+      });
+    }, 200);
+
     $('.tile').each( function(){
       var lspacer = $(this).data('lspacer'),
       rspacer = $(this).data('rspacer');
 
-      $(this).addClass('lspacer-' + lspacer + ' rspacer-' + rspacer)
+      var spacer = $('<li />', { "class": 'spacer',}),
+      spacer_double = $('<li />', { "class": 'tile spacer-double',});
+
+      if ($(this).data('lspacer') == 1) {
+        spacer.insertBefore( $( this ) );
+      }
+      if ($(this).data('lspacer') == 2) {
+        spacer_double.insertBefore( $( this ) );
+      }
+      if ($(this).data('rspacer') == 1) {
+        spacer.insertAfter( $( this ) );
+      }
+      if ($(this).data('rspacer') == 2) {
+        spacer_double.insertAfter( $( this ) );
+      }
     });
   }
 
